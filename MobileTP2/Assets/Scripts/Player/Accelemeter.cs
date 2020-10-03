bcf52a7812199540b36a986a35bfa493bc074e7e
Lautaro Coccia <lautaroauditore@gmail.com>
2020-10-03 20:44:18 -0300

    Camera movement

--- MobileTP2/Assets/Scripts/Player/Accelemeter.cs
+++ MobileTP2/Assets/Scripts/Player/Accelemeter.cs
@@ -6,6 +6,7 @@ public class Accelemeter : MonoBehaviour
 {
     public bool isFlat = true;
     private Rigidbody rigid;
+    
     private void Start()
     {
         rigid = GetComponent<Rigidbody>();
@@ -16,6 +17,7 @@ public class Accelemeter : MonoBehaviour
         Vector3 tilt = Input.acceleration;
         if (isFlat)
         {
+            
             tilt = Quaternion.Euler(90, 0, 0) * tilt;
         }
         rigid.AddForce(tilt);
